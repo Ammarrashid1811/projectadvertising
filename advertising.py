8a3df08f1b85ccc0db9df8f0c0c3a014661422ca
Ammarrashid1811 <161561164+Ammarrashid1811@users.noreply.github.com>
2024-02-28 16:02:43 +0800

    Update advertising.py

--- advertising.py
+++ advertising.py
@@ -11,12 +11,12 @@ This app predicts the **Advertising Sales** based on TV, Newspaper and Radio exp
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    TV = st.sidebar.slider('TV', 0.70, 300.0, 150.0)
-    Radio = st.sidebar.slider('Radio', 0, 50.0, 25.0)
-    Newspaper = st.sidebar.slider('Newspaper', 0.3, 115.0, 70.0)
-    data = {'TV': TV,
-            'Radio': Radio,
-            'Newspaper': Newspaper}
+    tv = st.sidebar.slider('TV', 0.70, 300.0, 150.0)
+    radio = st.sidebar.slider('Radio', 0, 50.0, 25.0)
+    newspaper = st.sidebar.slider('Newspaper', 0.3, 115.0, 70.0)
+    data = {'TV': tv,
+            'Radio': radio,
+            'Newspaper': newspaper}
     features = pd.DataFrame(data, index=[0])
     return features
 
