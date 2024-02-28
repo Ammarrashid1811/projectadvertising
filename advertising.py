fe0d0cc410c8713525106c2416b83c6a2cc39ec4
Ammarrashid1811 <161561164+Ammarrashid1811@users.noreply.github.com>
2024-02-28 16:26:34 +0800

    Update advertising.py

--- advertising.py
+++ advertising.py
@@ -1,1 +1,33 @@
- 
+import streamlit as st
+import pandas as pd
+import pickle
+
+st.write("""
+# Sales Prediction App
+
+This app predicts the **Advertising Sales** based on TV, Newspaper and Radio expenses!
+""")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    TV = st.sidebar.slider('TV', 0.70, 300.0, 150.0)
+    Radio = st.sidebar.slider('Radio', 0, 50.0, 25.0)
+    Newspaper = st.sidebar.slider('Newspaper', 0.3, 115.0, 70.0)
+    data = {'TV': TV,
+            'Radio': Radio,
+            'Newspaper': Newspaper}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+loaded_model = pickle.load(open("Advertisingmodel.h5", "rb"))
+
+prediction = loaded_model.predict(df)
+
+st.subheader('Prediction')
+st.write(prediction)
