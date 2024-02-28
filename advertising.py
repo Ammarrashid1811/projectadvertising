962c57bd50b37322c2e063cf53a6f086b6b72fa0
Ammarrashid1811 <161561164+Ammarrashid1811@users.noreply.github.com>
2024-02-28 16:36:25 +0800

    Update advertising.py

--- advertising.py
+++ advertising.py
@@ -5,15 +5,15 @@ import pickle
 st.write("""
 # Sales Prediction App
 
-This app predicts the **Advertising Sales** based on TV, Newspaper and Radio expenses!
+This app predicts the **Advertising Sales** type!
 """)
 
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    TV = st.sidebar.slider('TV', 0.70, 300.0, 150.0)
-    Radio = st.sidebar.slider('Radio', 0, 50.0, 25.0)
-    Newspaper = st.sidebar.slider('Newspaper', 0.3, 115.0, 70.0)
+    TV = st.sidebar.slider('TV', 0.7, 297.0, 100.0)
+    Radio = st.sidebar.slider('Radio', 0.0, 50.0, 15.0)
+    Newspaper = st.sidebar.slider('Newspaper', 0.3, 114.0, 20.0)
     data = {'TV': TV,
             'Radio': Radio,
             'Newspaper': Newspaper}
@@ -25,7 +25,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-loaded_model = pickle.load(open("Advertisingmodel.h5", "rb"))
+loaded_model = pickle.load(open("Advertising_model.h5", "rb"))
 
 prediction = loaded_model.predict(df)
 
